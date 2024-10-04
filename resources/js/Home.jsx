615f84fe148e4d2f43f4d88c79e1881c1f8defed
douglasderrick <douglasder12@gmail.com>
2024-10-04 10:02:04 +0000

    Initial commit: Laravel 11 with React, Vite, and Bootstrap setup

---
+++ resources/js/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () => {
+    return <div>
+        <p className="text-danger">Hello, world!</p>
+    </div>;
+};
+
+export default Home;
+
---
+++ resources/js/app.jsx
@@ -0,0 +1,9 @@
+import "./bootstrap";
+import "../css/app.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+
+import ReactDOM from "react-dom/client";
+import Home from "./Home";
+
+ReactDOM.createRoot(document.getElementById("app")).render(<Home />);
